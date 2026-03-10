702a54300dd74fb89684e2bd2d049fdc5b7a57f0
artskory <korygans@hotmail.fr>
2026-03-10 11:51:42 +0100

    suppression du bouton installer bookmarklet une fois installer

--- index.php
+++ index.php
@@ -4,6 +4,15 @@
  * Version 1.18 - Architecture MVC
  */
 
+// Ping bookmarklet : posé un cookie de longue durée et répond en JSON
+if (isset($_GET['ping-bookmarklet'])) {
+    setcookie('bookmarklet_installed', '1', time() + (365 * 24 * 3600), '/');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Origin: *');
+    echo json_encode(['status' => 'ok']);
+    exit;
+}
+
 require_once 'controllers/IndexController.php';
 
 $controller = new IndexController();
